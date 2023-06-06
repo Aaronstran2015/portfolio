bf8d3d03042b41bb7b233de7d4a14d24be7aa0a1
Aaron Stranathan <aaronstran@gmail.com>
2023-06-05 23:43:45 -0400

    first commit

---
+++ scripts/hamburger-overlay.js
@@ -0,0 +1,8 @@
+function toggle(element) 
+{
+    var hamburger = document.getElementById('hamburger')
+    var menu = document.getElementById('menu')
+    hamburger.classList.toggle('hamburger-toggle')
+    menu.classList.toggle('menu-toggle')
+
+}
